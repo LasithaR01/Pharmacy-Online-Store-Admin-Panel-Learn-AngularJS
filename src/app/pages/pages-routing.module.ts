b0b6e71d82e30fffe6cb4d88ffb854526781fb1f
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-06-30 08:06:02 +0530

    Update pages-routing.module.ts

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -47,6 +47,7 @@ const routes: Routes = [
   { path: 'restock-requests', loadChildren: () => import('./restock-requests/restock-requests.module').then(m => m.RestockRequestsModule) },
   { path: 'product-alternatives', loadChildren: () => import('./product-alternatives/product-alternatives.module').then(m => m.ProductAlternativesModule) },
   { path: 'drug-interactions', loadChildren: () => import('./drug-interactions/drug-interactions.module').then(m => m.DrugInteractionsModule) },
+  { path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) },
 
  ];
 
